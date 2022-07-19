f61d5fe314e4c8667d810d21077f0a560cca0281
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2022-07-19 22:24:05 +0200

    Added Eslint configuration with Airbnb.

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "@typescript-eslint", "jest"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"], // Your TypeScript files extension
+
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      extends: [
+        "airbnb-typescript",
+        "airbnb/hooks",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:jest/recommended",
+        "plugin:prettier/recommended",
+        "plugin:import/recommended",
+      ],
+
+      parserOptions: {
+        project: ["./tsconfig.json"], // Specify it only for TypeScript files
+      },
+    },
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  parserOptions: {
+    project: ["./tsconfig.json"],
+  },
+  rules: {
+    "linebreak-style": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+        "singleQuote": true,
+        "trailingComma": "all"
+      },
+    ],
+  },
+};
