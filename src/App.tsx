5512a04e6a7e348593f8a96aa93c726716bdd958
Marcin <marcinlacala@gmail.com>
2022-07-21 16:56:33 +0200

    Axios test and base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/login" element={<LoginView />} />
+          {/*user auth require*/}
+          <Route path="/" element={<RequireAuthUser />}>
+            <Route path="/cv" element={<CvView />} />
+            <Route path="/profile" element={<ProfileView />} />
+          </Route>
+          {/*HR auth require*/}
+          <Route path="/" element={<RequireAuthHr />}>
+            <Route path="/all-students" element={<AllStudentsView />} />
+            <Route
+              path="/interview-students"
+              element={<StudentsInterviewViews />}
+            />
+          </Route>
+          {/*Admin auth require*/}
+          <Route path="/" element={<RequireAuthAdmin />}>
+            <Route path="/admin-panel" element={<AdminPanelViews />} />
+          </Route>
+        </Route>
+      </Routes>
+    </>
   );
 }
 
