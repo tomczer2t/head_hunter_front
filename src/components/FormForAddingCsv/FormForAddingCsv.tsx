95bc89225508f35b789367f04e143c304dd6d598
Marcin <marcinlacala@gmail.com>
2022-07-23 20:38:35 +0200

    Create base component for Admin panel with css -Common/Header -FormForAddingHr -FormForAddingCsv

--- src/components/FormForAddingCsv/FormForAddingCsv.tsx
+++ src/components/FormForAddingCsv/FormForAddingCsv.tsx
@@ -4,5 +4,9 @@ import './FormForAddingCsv.css';
 export const FormForAddingCsv = () => {
   useEffect(() => {}, []);
 
-  return <div className="form-adding-csv"></div>;
+  return (
+    <div className="form-adding-csv">
+      <h3>Add HR</h3>
+    </div>
+  );
 };
