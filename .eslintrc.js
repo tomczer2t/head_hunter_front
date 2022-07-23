e31612460a253e036f7c51fdf658e108ea0e6b48
Tomasz Czerwiński <czerwitom@gmail.com>
2022-07-23 07:31:57 +0200

    Added rules to eslintrc.

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,6 +46,10 @@ module.exports = {
       {
         "ignore":['types'],
         "caseSensitive": false }
-    ]
+    ],
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-floating-promises": "off",
   },
 };
