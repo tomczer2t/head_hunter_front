35394507dd81fb4fd9788e74c5c249ec68788f3d
Marcin <marcinlacala@gmail.com>
2022-07-23 20:38:35 +0200

    Create base component for Admin panel with css -Common/Header -FormForAddingHr -FormForAddingCsv

--- src/components/FormForAddingHr/FormForAddingHr.tsx
+++ src/components/FormForAddingHr/FormForAddingHr.tsx
@@ -1,8 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './FormForAddingHr.css';
+import { Input } from '../common/Input/Input';
 
 export const FormForAddingHr = () => {
+  const [hrFormData, setHrFormData] = useState();
+
   useEffect(() => {}, []);
 
-  return <div className="form-adding-hr"></div>;
+  const changedHandle = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    return setHrFormData((loginParams: any) => ({
+      ...loginParams,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  return (
+    <div className="form-adding-hr">
+      <form className="form-adding-hr__form" action="">
+        <h3>Add HRV</h3>
+        <Input />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 };
