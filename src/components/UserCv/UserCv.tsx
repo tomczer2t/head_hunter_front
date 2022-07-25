4e9638a569a2c5a29114e7a5d7785d77b31b3704
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2022-07-25 22:21:47 +0200

    Add back to StudentsInterviewListView button and back-arrow.svg icon.

--- src/components/UserCv/UserCv.tsx
+++ src/components/UserCv/UserCv.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { TopBar } from '../TopBar/TopBar';
 
 import './UserCv.css';
+import { Link } from 'react-router-dom';
 
 export const UserCv = () => {
   return (
     <div className="UserCv">
       <TopBar />
+      <Link
+        className="UserCv__interview-student-link"
+        to={'/hr/interview-students'}
+      >
+        wróć
+      </Link>
     </div>
   );
 };
