ddcbee019f96d514471fda09f19ca21d18dfa037
RoJinx <mar.lewandowski@gmail.com>
2022-07-27 13:42:41 +0200

    little progres on redirecting - useNavigate

--- src/components/LoginPanel/LoginPanel.tsx
+++ src/components/LoginPanel/LoginPanel.tsx
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginPanel.css';
 import { Link } from 'react-router-dom';
+import { axios } from '../../api/axios';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginPanel = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const sendAction = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(email, password);
+    axios
+      .post('/auth/login', {
+        email,
+        password,
+      })
+      .then((response) => {
+        console.log(response);
+        navigate(`/hr/all-students`);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    // @Todo co ma się stać po rejestracji
+    // @Todo obsługa błędów
+  };
+
   return (
     <>
       <div className="login-panel">
         <img className="logo" src="/logo-megak.webp" alt="" />
-        <form>
+        <form onSubmit={sendAction}>
           <div className="container">
-            <input type="email" placeholder="E-mail" name="email" required />
+            <input
+              type="email"
+              placeholder="E-mail"
+              name="email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <input
               type="password"
               placeholder="Hasło"
               name="password"
               required
+              onChange={(e) => setPassword(e.target.value)}
             />
             <Link className="linkForgotPass" to="/">
               Zapomniałeś hasła?
