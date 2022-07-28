258d7431db09e3e5be5b1e8677c0b803ea583e0d
RoJinx <mar.lewandowski@gmail.com>
2022-07-28 18:41:16 +0200

    Search css progres

--- src/components/SearchFilterBar/SearchFilterBar.tsx
+++ src/components/SearchFilterBar/SearchFilterBar.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchFilterBar.css';
 import magnifier from '../../assets/images/magnifier.svg';
 
 export const SearchFilterBar = () => {
+  const [search, setSearch] = useState('');
+
   function searchHandler(e: React.FormEvent) {
     e.preventDefault();
   }
@@ -10,12 +12,15 @@ export const SearchFilterBar = () => {
   return (
     <div className="search-filter-bar">
       <div>
-        <form>
-          <img src={magnifier} className="search-filter-bar__magnifier" />
+        <form onSubmit={SearchFilterBar}>
+          <button className="search-filter-bar__bnt" onSubmit={SearchFilterBar}>
+            <img src={magnifier} className="search-filter-bar__magnifier" />
+          </button>
           <input
             type="text"
             placeholder="Szukaj"
             className="search-filter-bar__input"
+            onChange={(e) => setSearch(e.target.value)}
           />
         </form>
       </div>
