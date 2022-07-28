d97b65b776229b33f597cde6824b9c004a2b604f
RoJinx <mar.lewandowski@gmail.com>
2022-07-28 11:28:51 +0200

    start feature and copy starters -images -TopBar from feature-05

---
+++ src/components/HrAllStudents/HrAllStudents.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+export const HrAllStudents = () => {
+  return (
+    <>
+      <p>aaaa</p>
+    </>
+  );
+};
--- src/views/AllStudentsView.tsx
+++ src/views/AllStudentsView.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { TopBar } from '../components/TopBar/TopBar';
 export const AllStudentsView = () => {
   return (
     <>
+      <TopBar />
       <h1>AllStudentsView</h1>
     </>
   );
