3e207952a15d116f8b604a32207f418f92e0cd05
Adrian Majcher <majcherrr87@gmail.com>
2022-08-02 22:08:38 +0200

    Add form to component

--- src/components/StudentProfile/StudentProfile.tsx
+++ src/components/StudentProfile/StudentProfile.tsx
@@ -1,30 +1,111 @@
 export const StudentProfile = () => {
-    return (
-        <>
-            <div className='content'>
-                <h2>Profil Kursanta</h2>
-                <form>
-                    <label>Imie:</label>
-                    <input
-                        type="text"
-                        required
-                    />
-                    <label>Nazwisko:</label>
-                    <input
-                        type="text"
-                        required
-                    />
-                    <label>długi text</label>
-                    <textarea
-                        required
-                    ></textarea>
-                    <label>Wybór:</label>
-                    <select>
-                        <option value="jeden">jeden</option>
-                        <option value="dwa">dwa</option>
-                    </select>
-                </form>
+  return (
+    <>
+      <div className="container">
+        <h2>
+          Profil Kursanta
+          <img
+            src="https://avatars.githubusercontent.com/u/26481499?v=4"
+            alt=""
+          />
+        </h2>
+        <form>
+          <div className="form-body">
+            <div className="form-col">
+              <label>
+                Imie: <p>bład</p>
+              </label>
+              <input type="text" required />
+
+              <label>Nazwisko:</label>
+              <input type="text" required />
+
+              <label>Email:</label>
+              <input type="email" required />
+
+              <label>Tel:</label>
+              <input type="number" />
+
+              <label>Konto GitHub:</label>
+              <input type="text" />
+
+              <label>Bio:</label>
+              <textarea></textarea>
             </div>
-        </>
-    )
-}
+            <div className="form-col">
+              <label>Miasto pracy</label>
+              <input type="text" />
+
+              <label>Oczekiwanie wynagrodzenie netto</label>
+              <input type="number" />
+
+              <label>Preferowane miejsce pracy:</label>
+              <select>
+                <option value="jeden">Na miejscu</option>
+                <option value="dwa">Gotowość do przeprowadzki</option>
+                <option value="dwa">Wyłącznie zdalnie</option>
+                <option value="dwa" selected>
+                  Bez znaczenia
+                </option>
+              </select>
+
+              <label>Oczekiwany typ kontraktu:</label>
+              <select>
+                <option value="jeden">Tylki UoP</option>
+                <option value="dwa">Możliwość B2B</option>
+                <option value="dwa">Możliwość UZ/UoP</option>
+                <option value="dwa" selected>
+                  Brak preferencji
+                </option>
+              </select>
+
+              <label>Ilość miesięcy doświadczenia komercyjnego</label>
+              <input type="number" />
+
+              <label>Przebieg edukacji:</label>
+              <textarea></textarea>
+            </div>
+            <div className="form-col">
+              <label>Zgoda na odbycie bezpłatnych praktyk:</label>
+              <select>
+                <option value="jeden">Tak</option>
+                <option value="dwa" selected>
+                  Nie
+                </option>
+              </select>
+
+              <div className="form-url">
+                <label>URL do portfolio</label>
+                <input type="text" />
+                <button className="url-bnt">Dodaj</button>
+                <div className="url-list">
+                  <p>url</p>
+                  <p>url</p>
+                  <p>url</p>
+                </div>
+              </div>
+
+              <div className="form-url">
+                <label>URL do porojektu zaliczeniowego na GitHub</label>
+                <input type="text" />
+                <button className="url-bnt">Dodaj</button>
+                <div className="url-list">
+                  <p>url</p>
+                  <p>url</p>
+                  <p>url</p>
+                </div>
+              </div>
+
+              <label>Przebieg doświadczenia zawodowego:</label>
+              <textarea></textarea>
+
+              <label>Kursy i certyfikaty związane z programowanie:</label>
+              <textarea></textarea>
+            </div>
+          </div>
+          <button className="subminBnt">Wyślij</button>
+        </form>
+      </div>
+    </>
+  );
+};
