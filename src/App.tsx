05ba59085e861cb3c64150bb6d27941696418aaa
Marcin <marcinlacala@gmail.com>
2022-08-03 14:27:32 +0200

    Something is wrong :(

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
           </Route>
           {/*HR auth require*/}
           <Route path="hr" element={<RequireAuthHr />}>
-            <Route path="all-students" element={<AllStudentsView />} />
+            <Route path="students" element={<AllStudentsView />} />
             <Route
               path="interview-students"
               element={<StudentsInterviewListViews />}
@@ -40,7 +40,7 @@ function App() {
           </Route>
           {/*Admin auth require*/}
           <Route path="admin" element={<RequireAuthAdmin />}>
-            <Route path="admin-panel" element={<AdminPanelViews />} />
+            <Route path="panel" element={<AdminPanelViews />} />
           </Route>
         </Route>
       </Routes>
--- src/components/FormForAddingCsv/FormForAddingCsv.tsx
+++ src/components/FormForAddingCsv/FormForAddingCsv.tsx
@@ -1,24 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './FormForAddingCsv.css';
+import { MessageResponse } from '../common/MessageResponse/MessageResponse';
+import axiosDefault, { AxiosResponse } from 'axios';
+import { AddStudentsResponse } from 'types';
 
 export const FormForAddingCsv = () => {
-  useEffect(() => {}, []);
+  // const [selectedFile, setSelectedFile] = useState(null);
+  const [showMessageResponse, setShowMessageResponse] = useState(false);
 
-  const sendForm = () => {};
+  const [selectedFile, setSelectedFile] = useState<FormData>({} as FormData);
+
+  const sendForm = async () => {
+    if (selectedFile) {
+      const res: AxiosResponse<AddStudentsResponse> = await axiosDefault.post(
+        '/admin/import-students',
+        {
+          students: selectedFile,
+        },
+      );
+      console.log(res);
+    } else {
+      console.log('nie wybrałeś pliku');
+    }
+  };
+
+  const handleFileSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files[0]) return setSelectedFile(null);
+    const formData = new FormData();
+    // console.log(e.target.files[0]);
+    // console.log(e.target.files[0].name);
+    formData.append('file', e.target.files[0], e.target.files[0].name);
+
+    setSelectedFile(formData);
+  };
 
   return (
     <div className="form-adding-csv">
-      <form
-        onSubmit={() => {
-          void sendForm();
-        }}
-        className="form-adding-csv__form"
-      >
+      <MessageResponse
+        showMessageResponse={showMessageResponse}
+        status={'Testowy'}
+        message={'Opis błedu'}
+        correct={false}
+        closeMessage={setShowMessageResponse}
+      />
+      <form onSubmit={void sendForm} className="form-adding-csv__form">
         <h3 className="form-adding-csv__form__title">
           Dodawanie CSV z Kursantami
         </h3>
-        <input type="file" />
-        <button>Wyślij plik CSV</button>
+        <input
+          type="file"
+          accept=".csv"
+          onChange={(e) => handleFileSelect(e)}
+        />
+        <button type="submit">Wyślij plik CSV</button>
       </form>
     </div>
   );
