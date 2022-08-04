7aface05f4cf8c7627c621276ce5d14c924e8afd
Marcin <marcinlacala@gmail.com>
2022-08-04 02:23:31 +0200

    Fixes MessageResponse.tsx and style

--- src/components/common/MessageResponse/MessageResponse.tsx
+++ src/components/common/MessageResponse/MessageResponse.tsx
@@ -2,22 +2,8 @@ import React, { Dispatch, useEffect } from 'react';
 import { AddStudentsResponse } from 'types';
 import './MessageResponse.css';
 
-// {
-//   "failedFor": [],
-//   "updatedFor": [
-//   "example@zaqw.pl",
-//   "jakj2st@o2.pl",
-//   "coZt@gmail.com",
-//   "trzeci@gmail.com",
-//   "jakisziom@gmail.com"
-// ],
-//   "failedCount": 0,
-//   "message": "Successfully imported and registered 5 students out of a possible 5.",
-//   "successfullyUpdated": 5,
-//   "successfullyAdded": 0
-// }
-
 interface Props {
+  message: string;
   data?: AddStudentsResponse | null;
   showMessageResponse: boolean;
   closeMessage: Dispatch<React.SetStateAction<boolean>>;
@@ -25,34 +11,51 @@ interface Props {
 
 export const MessageResponse = (props: Props) => {
   useEffect(() => {}, [props.data]);
-
   if (!props.showMessageResponse) return null;
 
-  const mailsFailed = [];
-  const mailsUpdate = [];
+  let mailsFailed = [];
+  let mailsUpdate = [];
+
+  console.log(props.data);
 
   if (props.data) {
-    const failedForArray = props.data.failedFor.entries();
     for (let i = 0; i < props.data.failedFor.length; i++) {
       mailsFailed.push(<p>{`${props.data.failedFor[i].email}`}</p>);
     }
     for (let i = 0; i < props.data.updatedFor.length; i++) {
       mailsUpdate.push(<p>{`${props.data.updatedFor[i]}`}</p>);
     }
+  } else {
+    mailsFailed = [];
+    mailsUpdate = [];
   }
 
   return (
     <div className="message-response__blur">
-      <div className={`message-response`}>
-        <div className="message-response__message">
-          <h4>Dodano: {props.data?.successfullyAdded} maili</h4>
-          <h4>Niewłaściwe: {props.data?.failedCount} maili</h4>
-          {mailsFailed}
-        </div>
-        <div className="message-response__message">
-          <h4>Zaktualizowano: {props.data?.successfullyUpdated} maili</h4>
-          {mailsUpdate}
-        </div>
+      <div
+        className={`message-response`}
+        style={{ height: `${props.data ? '60vh' : '20vh'}` }}
+      >
+        <h3>{props.message}</h3>
+        {props.data ? (
+          <div className={`message-response__box-helper`}>
+            <div className="message-response__message">
+              <h4>Dodano: {props.data?.successfullyAdded} maili</h4>
+            </div>
+            <hr />
+            <div className="message-response__message">
+              <h4>Niewłaściwe: {props.data?.failedCount} maili</h4>
+              {mailsFailed}
+            </div>
+            <hr />
+            <div className="message-response__message">
+              <h4>Zaktualizowano: {props.data?.successfullyUpdated} maili</h4>
+              {mailsUpdate}
+            </div>
+            <hr />
+          </div>
+        ) : null}
+
         <button
           className="message-response__close-message"
           onClick={() => {
