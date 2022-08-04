c00a5dfdc334f233c890fca38a0fc17a33551323
Marcin <marcinlacala@gmail.com>
2022-08-04 06:57:14 +0200

    Added redirect from home page to loginView

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,5 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useCallback, useEffect } from 'react';
 export const MainLayout = () => {
+  const navigate = useNavigate();
+  const redirect = useCallback(() => {
+    navigate('/login');
+  }, [navigate]);
+
+  useEffect(() => {
+    redirect();
+  }, [redirect]);
   return (
     <>
       <Outlet />
