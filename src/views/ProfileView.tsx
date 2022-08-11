16f26276a2081ed7957536a28b967fb7c8a33f84
Marcin <marcinlacala@gmail.com>
2022-08-11 16:22:49 +0200

    Hotfixes with structure

--- src/views/ProfileView.tsx
+++ src/views/ProfileView.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import { UserProfileForm } from '../components/UserProfileForm/UserProfileForm';
 import { useAuth } from '../hooks/useAuth';
 import { UserRole } from 'types';
+import { StudentProfile } from '../components/StudentProfile/StudentProfile';
+import { TopBar } from '../components/TopBar/TopBar';
+
 export const ProfileView = () => {
   const { auth } = useAuth();
-  return auth?.role === UserRole.STUDENT ? <UserProfileForm /> : null;
+  return auth?.role === UserRole.STUDENT ? (
+    <>
+      <TopBar />
+      <StudentProfile />
+    </>
+  ) : null;
 };
