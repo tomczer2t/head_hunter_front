2a4f3aef06c088f2f3d2e30dfebea12bd0d98341
Tomasz Czerwiński <czerwitom@gmail.com>
2022-08-23 13:38:14 +0200

    Displaying specific error in ListStudentsForBooking component.

--- src/components/ListStudentsForBooking/ListStudentsForBooking.tsx
+++ src/components/ListStudentsForBooking/ListStudentsForBooking.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SingleStudent } from './SingleStudent/SingleStudent';
 import './ListStudentsForBooking.css';
 import { FilteredAvailableStudent } from 'types';
+import { MessageResponse } from '../common/MessageResponse/MessageResponse';
 
 interface Props {
   students: FilteredAvailableStudent[];
 }
 
 export const ListStudentsForBooking = (props: Props) => {
+  const [error, setError] = useState('');
+
   return (
     <>
       <div className="list-students-for-booking__wrapper">
         <ul className="list-students-for-booking">
           {props.students.map((student) => (
-            <SingleStudent student={student} key={student.userId} />
+            <SingleStudent
+              setError={setError}
+              student={student}
+              key={student.userId}
+            />
           ))}
         </ul>
       </div>
+      <MessageResponse
+        closeMessage={() => setError('')}
+        message={error}
+        showMessageResponse={!!error}
+      />
     </>
   );
 };
